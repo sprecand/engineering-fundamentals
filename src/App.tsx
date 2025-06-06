aa398e728a69ecd89255753bb8c888ef4bb5f93a
Stephan <github.carat974@passmail.net>
2025-06-06 13:45:45 +0200

    Cleanup App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,6 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
-  const redundantData = "This variable is here for no good reason.";
-
   return (
     <>
       <div>
