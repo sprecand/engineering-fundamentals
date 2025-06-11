d06e2af3315a17aca0fb79673518f67bb5a9bf19
benrow4 <benrow@protonmail.com>
2025-06-11 13:03:07 +0200

    feature(react-app): improve Part A - Unit test

---
+++ src/__tests__/Counter.test.tsx
@@ -0,0 +1,6 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import Counter from "../Counter";
+
+test("increments count when button is clicked", () => {
+    // Add the test logic below
+});
