329a91c49d850c838db0e803b67ba6a4f38ac9ef
benrow4 <benrow@protonmail.com>
2025-06-16 17:31:12 +0200

    Remove the build errors from the Counter.test.tsx

--- src/__tests__/Counter.test.tsx
+++ src/__tests__/Counter.test.tsx
@@ -1,6 +1,7 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import { render} from "@testing-library/react";
 import Counter from "../Counter";
 
 test("increments count when button is clicked", () => {
+    render(<Counter />);
     // Add the test logic below
 });
