2c8a70fc5cad6d85ea19361e6ec3c7e556dae0bd
Andreas Sprecher <75544588+sprecand@users.noreply.github.com>
2025-06-17 09:39:48 +0000

    fix(part-d): removed unused var

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,6 @@ import Logo from "./Logo";
 import Counter from "./Counter";
 
 function App() {
-
-  // @ts-ignore
-  const redundantData = "This variable is here for no good reason.";
-
   return (
     <>
       <Logo />
